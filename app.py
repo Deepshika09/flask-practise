f3a15dcd19435a5a7671e546bc1545893735cf61
Deepshika H <deepshikavhk@gmail.com>
2026-07-03 23:12:49 +0530

    Day 2: Added Flask routing and Jinja2 templates

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello World!"
+    return render_template(
+        "index.html",
+        title="Home",
+        username="Deepshika",
+        message="Welcome to my first Flask website!"
+    )
+
+@app.route("/about")
+def about():
+    return render_template(
+        "about.html",
+        title="About",
+        college="SEC",
+        course="B.Tech Information Technology"
+    )
 
 if __name__ == "__main__":
     app.run(debug=True)
